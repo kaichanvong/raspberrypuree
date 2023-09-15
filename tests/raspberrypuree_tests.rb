6cac507a20db08188ae25e1e9abc22457b28ba53
Kai Vong <kai.vong@gmail.com>
2023-09-15 09:26:21 +0100

    Update raspberrypuree_tests.rb

--- tests/raspberrypuree_tests.rb
+++ tests/raspberrypuree_tests.rb
@@ -3,23 +3,24 @@ require_relative "../raspberrypuree.rb"
 
 class TestRaspberrypuree < Minitest::Test
 	def setup
-		@puree = Raspberrypuree.new({site_url:'https://github.com', text: ' here is a example from the user @kaichanvong about his user info of copied text '})
+		params = { site_url:'https://github.com', 
+			   text: 'here is a example from the user @kaicvong about his user info of copied text' }
+		@puree = Raspberrypuree.new(params)
 	end
 
 	def test_markdown
-		assert_equal('[@kaichanvong](https://github.com/kaichanvong)', @puree.markdown)
+		assert_equal('[@kaicvong](https://github.com/kaicvong)', @puree.markdown)
 	end
 
 	def test_uri
-		assert_equal('https://github.com/kaichanvong', @puree.uri)
+		assert_equal('https://github.com/kaicvong', @puree.uri)
 	end
 
 	def test_username
-		assert_equal('kaichanvong', @puree.username)
+		assert_equal('kaicvong', @puree.username)
 	end
 
 	def test_firstname
 		assert_equal('kai', @puree.nickname) # I mean there is CSS property and value: `text-transform: capitalize`;
 	end
-
 end
