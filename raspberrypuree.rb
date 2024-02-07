bdfd7bed48d3d27e50df784ce64735d4224ff044
kaichanvong <kai.vong@gmail.com>
2024-02-07 22:48:31 +0000

    add more tests

--- raspberrypuree.rb
+++ raspberrypuree.rb
@@ -1,3 +1,5 @@
+KNOWN_SITES = [ { name: 'github', link: 'https://github.com'  } ]
+
 class Raspberrypuree
 	attr_reader :markdown, :username, :uri, :nickname
 
@@ -5,16 +7,20 @@ class Raspberrypuree
 		site_url = options[:site_url]
 		text = options[:text]
 		@raspberrymark = '@'
-		@username = find_user(text)
-		@nickname = nicknames_finder(@username)
+    if options[:form_completed]      
+      @username = options[:username]
+      @nickname = options[:nickname]  
+    else
+      @username = find_user(text)
+      @nickname = nicknames_finder(@username)
+    end
 		@uri = "#{site_url}/#{@username.downcase}" # Quack!
 	end
 
 	def nicknames_finder(username)
-		nickname_length = 3
 		possible_nicknames_known = %w{kai}
 		nick_names = possible_nicknames_known.select do |nickname|
-			nickname if username.include?(nickname) # && nickname.length > nickname_length
+			nickname if username.include?(nickname)
 		end
 		nick_names.compact.last
 	end
--- tests/raspberrypuree_tests.rb
+++ tests/raspberrypuree_tests.rb
@@ -3,24 +3,33 @@ require_relative "../raspberrypuree.rb"
 
 class TestRaspberrypuree < Minitest::Test
 	def setup
-		params = { site_url:'https://github.com', 
-			   text: 'here is a example from the user @kaichanvong about his user info of copied text' }
-		@puree = Raspberrypuree.new(params)
+		params1 = { site_url: 'https://github.com', 
+               form_completed: true,
+               username: 'kaichanvong',
+               nickname: 'kai' }
+    params2 = { site_url: 'https://github.com',
+                text: 'here is a example from the user @kaichanvong about his user info of copied text' }
+    @puree1 = Raspberrypuree.new(params1)
+		@puree2 = Raspberrypuree.new(params2)
 	end
 
 	def test_markdown
-		assert_equal('[@kaichanvong](https://github.com/kaichanvong)', @puree.markdown)
+    assert_equal('[@kaichanvong](https://github.com/kaichanvong)', @puree1.markdown)
+		assert_equal('[@kaichanvong](https://github.com/kaichanvong)', @puree2.markdown)
 	end
 
 	def test_uri
-		assert_equal('https://github.com/kaichanvong', @puree.uri)
+    assert_equal('https://github.com/kaichanvong', @puree1.uri)
+		assert_equal('https://github.com/kaichanvong', @puree2.uri)
 	end
 
 	def test_username
-		assert_equal('kaichanvong', @puree.username)
+    assert_equal('kaichanvong', @puree1.username)
+		assert_equal('kaichanvong', @puree2.username)
 	end
 
 	def test_firstname
-		assert_equal('kai', @puree.nickname) # I mean there is CSS property and value: `text-transform: capitalize`;
+    assert_equal('kai', @puree1.nickname) # its possible to discover nicknames including emojis + more on different sites
+		assert_equal('kai', @puree2.nickname)
 	end
 end
